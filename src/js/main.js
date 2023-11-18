811421705c6654495ca0b731d65faa98642a52ca
Niccar95 <niccar.95@outlook.com>
2023-11-18 20:16:38 +0100

    funktionell lista klar, och har börjat med variabler i sass

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,11 @@
 import { TodoList } from "../models/todolist";
 import "../scss/style.scss";
 
-
 const listProperties = new TodoList("Leg day at the gym", "Monday", "18:00");
 const listProperties2 = new TodoList("Buy groceries", "Tuesday", "17:00");
 const listProperties3 = new TodoList("Clean the kitchen", "Wednesday", "19:00");
 const listProperties4 = new TodoList("Study Javascript", "Thursday", "18:00");
 const listProperties5 = new TodoList("Chill", "Friday", "20:00");
-
-
 
 
  const initialList = [listProperties, listProperties2, listProperties3, listProperties4, listProperties5];
@@ -18,6 +15,11 @@ const listProperties5 = new TodoList("Chill", "Friday", "20:00");
  localStorage.setItem("listInfo", JSON.stringify(myTodoList));
 
 const mainContainer = document.getElementById("app");
+const title = document.createElement("h1");
+
+mainContainer.appendChild(title);
+
+title.innerHTML = "To do list"
 
 
 function loadHtml() {
@@ -37,33 +39,36 @@ myTodoList.forEach((myList) => {
 
   doneButton.innerHTML = "Click to mark as done";
   undoButton.innerHTML = "Click to undo changes";
-
-  list.innerHTML = myList.task +": " + myList.day + " " + myList.time;
+    
+  list.innerHTML = myList.task +" - " + myList.day + " " + myList.time;
 
   if (myList.done) {
     list.innerHTML = "Done! &#10003;";
+    doneButton.hidden = true;
+    undoButton.hidden = false;
   }
 
+  else(
+    doneButton.hidden = false, undoButton.hidden = true
+  )
 
   doneButton.addEventListener("click", () => {
-
+    undoButton.hidden = false;
+    doneButton.hidden = true;
     myList.done = true;
     localStorage.setItem("listInfo", JSON.stringify(myTodoList));
     list.innerHTML = "Done! &#10003;";
-
-    
   })
 
   undoButton.addEventListener("click", () => {
-
+    undoButton.hidden = true;
+    doneButton.hidden = false;
     myList.done = false;
     localStorage.setItem("listInfo", JSON.stringify(myTodoList));
-    list.innerHTML = myList.task +": " + myList.day + " " + myList.time;
-    
+    list.innerHTML = myList.task +" - " + myList.day + " " + myList.time;
   })
 
 });
-
 }
 
 loadHtml();
